8b75217624067081d9366e14666dd8abcffca627
Petunia Stemere <133222271+Petuniapet@users.noreply.github.com>
2024-03-29 09:10:20 +0200

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+"""Fetches https://alx-intranet.hbtn.io/status and displays the response"""
+
 import urllib.request
 
-
 if __name__ == "__main__":
-    """fetches https://intranet.hbtn.io/status"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        html_str = html.decode('utf-8')
-
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html_str))
+    url = "https://alx-intranet.hbtn.io/status"
+    
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", utf8_content)
