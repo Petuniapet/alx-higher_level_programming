46a90393e4b010a1a9bef7a1cb9904c45e35d302
Petunia Stemere <133222271+Petuniapet@users.noreply.github.com>
2024-03-29 08:34:30 +0200

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/5-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import requests
-import sys
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-    response = requests.get(url)
-    
-    print(response.headers['X-Request-Id'])
