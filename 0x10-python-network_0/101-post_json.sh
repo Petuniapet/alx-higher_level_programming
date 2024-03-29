0e83a8de8a9208d5ec1751af7edf476abca82f8c
Petunia Stemere <petuniastemere@gmail.com>
2024-03-29 03:38:50 -0700

    changes

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a URL with the contents of a file and displays the body of the response
-json_file="$2"
-response=$(curl -s "$1" -X POST -H "Content-Type: application/json" --data-binary "@$json_file")
-if [[ $response == *"Valid JSON"* ]]; then
-    echo "Valid JSON"
-else
-    echo "Not a valid JSON"
-fi
+# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sX POST $1 -H "Content-Type: application/json" -d @$2 -L
